d5d0e7834c543efba51f511e9ba5ad6836e97d0e
Luke <lavedon@gmail.com>
2023-05-02 17:02:59 -0400

    updates

--- src/app/my-overlay.service.ts
+++ src/app/my-overlay.service.ts
@@ -2,6 +2,18 @@ import { Injectable } from '@angular/core';
 import { Overlay } from '@angular/cdk/overlay';
 import { ComponentPortal } from '@angular/cdk/portal';
 import { ComponentForDialogComponent } from './component-for-dialog/component-for-dialog.component';
+import { OverlayConfig }  from '@angular/cdk/overlay';
+
+
+interface DialogConfig {
+  hasBackdrop?: boolean;
+  panelClass?: string;
+}
+
+const DEFAULT_CONFIG: DialogConfig = {
+  hasBackdrop: false,
+  panelClass: 'dialog-panel'
+}
 
 @Injectable({
   providedIn: 'root'
@@ -10,11 +22,34 @@ export class MyOverlayService {
 
   constructor(private overlay: Overlay) { }
 
-  open() {
-    const overlayRef = this.overlay.create();
-
+  open(config: DialogConfig = {}) {
     const overlayPortal = new ComponentPortal(ComponentForDialogComponent)
+    const dialogConfig = {...DEFAULT_CONFIG, ...config};
+    const overlayRef = this.createOverlay(dialogConfig);
+
     overlayRef.attach(overlayPortal);
   }
+
+  private getOverlayConfig(config: DialogConfig): OverlayConfig {
+    const positionStrategy = this.overlay.position().
+      global()
+      .centerHorizontally()
+      .centerVertically();
+
+    const overlayConfig = new OverlayConfig({
+      hasBackdrop: config.hasBackdrop,
+      panelClass: config.panelClass,
+      scrollStrategy: this.overlay.scrollStrategies.block(),
+      positionStrategy
+    });
+
+    return overlayConfig;
+    }
+
+    private createOverlay(config: DialogConfig) {
+      const overlayConfig = this.getOverlayConfig(config);
+      return this.overlay.create(overlayConfig);
+    }
+
 
 }
